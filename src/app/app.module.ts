b758cf746b8386bd345a102672ae1dcf8e706e65
MinxianLi <lmx423@gmail.com>
2017-07-29 17:46:00 -0500

    commit working heroes app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,44 @@
+import { HeroSearchComponent } from './hero-search.component';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {HttpModule} from '@angular/http';
+
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+import { HeroService } from './hero.service';
+
+
+
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+    BrowserModule,
+    FormsModule,
+   AppRoutingModule,
+   HttpModule,
+
+   InMemoryWebApiModule.forRoot(InMemoryDataService)
+],
+  declarations: [ 
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroSearchComponent 
+  ],
+  providers: [
+    HeroService
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
